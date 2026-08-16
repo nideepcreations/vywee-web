3d371d11d5998699291b2abb263de5f0f00b92ab
nideepcreations <nideepcreations@gmail.com>
2026-08-16 23:04:48 +0530

    footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -16,7 +16,7 @@ function Footer() {
 
   return (
     <footer className="mt-auto border-t border-border bg-surface">
-      <Container className="py-12 md:py-16">
+      <Container size="wide" className="py-12 md:py-16">
         <div className="grid gap-10 md:grid-cols-[1.4fr_repeat(3,1fr)]">
           <div className="flex flex-col gap-4">
             <Logo />
