2967dc58836bb95671b6a5b71a603147a5730a9b
nideepcreations <nideepcreations@gmail.com>
2026-08-18 20:55:56 +0530

    vywee-choice.tsx

--- components/features/home/vywee-choice.tsx
+++ components/features/home/vywee-choice.tsx
@@ -38,8 +38,8 @@ function VyweeChoice() {
       <ul
         className={[
           '-mx-[var(--space-gutter)] flex snap-x snap-mandatory gap-4 overflow-x-auto px-[var(--space-gutter)] pb-2',
-          'md:mx-0 md:grid md:grid-cols-2 md:overflow-visible md:px-0 md:pb-0',
-          'lg:grid-cols-4',
+          'md:mx-0 md:grid md:grid-cols-2 md:gap-5 md:overflow-visible md:px-0 md:pb-0',
+'lg:grid-cols-4 lg:gap-5',
         ].join(' ')}
       >
         {picks.map((product, index) => (
