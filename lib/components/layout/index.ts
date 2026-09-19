333d896362beae8ebbb6e1b7feccb9203a4115a2
nideep <nideepcreations@gmail.com>
2026-09-19 21:46:22 +0530

    Revert "Update brand logo to Minimal Elegant mark"
    This reverts commit ab2e098fe4991e5bdd8c73d2e6e93bc636d0548c.

--- lib/components/layout/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './container';
-export * from './footer';
-export * from './header';
-export * from './main-nav';
-export * from './mobile-nav';
-export * from './page-wrapper';
-export * from './section';
